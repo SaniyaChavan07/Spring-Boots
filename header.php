6773af81101974cfa821b6e5f464878ca7defd8f
SaniyaChavan07 <149809216+SaniyaChavan07@users.noreply.github.com>
2025-01-30 21:03:54 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SpringBoots</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.css" integrity="sha512-GmZYQ9SKTnOea030Tbiat0Y+jhnYLIpsGAe6QTnToi8hI2nNbVMETHeK4wm4MuYMQdrc38x+sX77+kVD01eNsQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.css" integrity="sha512-GmZYQ9SKTnOea030Tbiat0Y+jhnYLIpsGAe6QTnToi8hI2nNbVMETHeK4wm4MuYMQdrc38x+sX77+kVD01eNsQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/brands.css" integrity="sha512-wiCAcyn1Z9FOqqMFKuerxELrSZr3TtHJvH4M9xtCRIMr/ydl9mptldoSP789oYknOdeaaDnd/CTKP0XCsqIFzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/brands.min.css" integrity="sha512-58P9Hy7II0YeXLv+iFiLCv1rtLW47xmiRpC1oFafeKNShp8V5bKV/ciVtYqbk2YfxXQMt58DjNfkXFOn62xE+g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/fontawesome.css" integrity="sha512-6R+/aZAbKDxn6/g+k2O68ulW3bD26ELehXubrIPM1vUfIXETpTdT4+Or6v53tNJcNkcyu4zxs0Cu9kkJAit0YQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/fontawesome.min.css" integrity="sha512-v8QQ0YQ3H4K6Ic3PJkym91KoeNT5S3PnDKvqnwqFD1oiqIl653crGZplPdU5KKtHjO0QKcQ2aUlQZYjHczkmGw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/regular.css" integrity="sha512-M/FJqWoATldJm2aBKIM8rHWfz1SEOy3sMumogiYtbE2YIkMVf6uyaPeIKjMHalSsvvIUwlAiTS3AisqGPoEJYw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/regular.min.css" integrity="sha512-8hM9a+2hrLBhOuB3uiy+QIXBsu6Qk+snsP1CboFQW6pdt/yYz0IcDp/+CGv5m39r9doGUc/zw6aBpyLF6XFgzg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/solid.css" integrity="sha512-CEvJpnAxszIPE6CPJO2tWRwI1d4zp+XbRC4LO4k/zyL8UI3VUZ0H3Vftot3zGO9W87ZWuBtuYJrECbNrELSapA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/solid.min.css" integrity="sha512-DzC7h7+bDlpXPDQsX/0fShhf1dLxXlHuhPBkBo/5wJWRoTU6YL7moeiNoej6q3wh5ti78C57Tu1JwTNlcgHSjg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/svg-with-js.css" integrity="sha512-yIVU1i2mwax5Eo+jEjuu2lq624zrD1v8XCi/cvzvYaOcJQUS7y4UkTRNOo+PDqwzKZX0t7am7xAqSErxBlqH1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/svg-with-js.min.css" integrity="sha512-vtbctC7BecZMHWPWej78RfcB9RmGpIx+G4+1IT2/Z9P7SIXApaI1XLOCrzpSKgNyTiw1VCmg/EkJXGo+LJYcpw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/v4-font-face.css" integrity="sha512-g7AczECEftqdaPPiG0fQ7qmaJJMr0oD9Fss8Ql3z8n8mPydLfFzQGzHY3JXWPpIpLjsSlJmak6yCZS39iGEJqw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/v4-font-face.min.css" integrity="sha512-nzYdV58RidllZqhgtA0a+ghpP5e8Qq3oNNBXNp+ShDoU0FUxwRmEjQzsL72SShhjX2dcLeqpyZ/MDW43hWHo4w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/v4-shims.css" integrity="sha512-8EiIGjMeIFqvBL6Fd4jdkKwwUZDdhcSlVj0+SKxt3oU9KFt5/ZyiS+vPbTrDRR1Z0K81/VwHCKpqTmU5PNbTuA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/v5-font-face.css" integrity="sha512-P/yBSiyfiUIyQqe2b7sKEy2VkLs3JSwNnO0IZDHIuJcVL/7HQFcv4fxuHQjKUcPFOdFfGoFOH83x5GYPpd8Kfg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/v5-font-face.min.css" integrity="sha512-dC4AauJwDHOpEpJi2/BsDf/b9vyLVt79nmO0x5gQWbRm62Q/xFS9iZpzCOFNLjTQsAD4mp9jJel6nPAjnBN2Ag==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.js" integrity="sha512-Nxw1drpjN57DlFRzn8h+RaS7dTjhPOlHftsKYGRkCEj1lEbx3k1bqNz1qvLpRkEF19qe7YSHppCdnLc8FUmfnQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js" integrity="sha512-b+nQTCdtTBIRIbraqNEwsjB6UvL3UEMkXnhzd8awtCYh0Kcsjl9uEgwVFVbhoj3uu1DO1ZMacNvLoyJJiNfcvg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/brands.js" integrity="sha512-bbYc5g9O6b9TcnN6mLrRMZTja59kog26CsegC/B1XlGEPJSXeHvfYNbBms3r4QE8dbscc9E/LYqgR8knTjw8SA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/brands.min.js" integrity="sha512-ILOQokRQNF0S8SKV6fnaLNj02CmZnDWmYUr3zlH4jwToep0lWc7twuTzF+Mm0cKPdszi0xe8KymVi2y7mAweVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/conflict-detection.js" integrity="sha512-fQ38epZheuGIO14iKZ2+45LjBBpT7fy6HS/Q+U/hsZyhy0yjrTOgMwndBwUPb3b0S1Cah0vQ6/68wYF36x7qww==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/conflict-detection.js" integrity="sha512-fQ38epZheuGIO14iKZ2+45LjBBpT7fy6HS/Q+U/hsZyhy0yjrTOgMwndBwUPb3b0S1Cah0vQ6/68wYF36x7qww==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/conflict-detection.min.js" integrity="sha512-K5+wFlOsuophh3a/Im5Xc/i3w5YnOJmfTS74GUNHUH0UPe2Y55Y8iLkFkLjYy6aJy999ZIoKjsmFJMhjq3MlHQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/fontawesome.js" integrity="sha512-gak8X5xIR1YqsvvmgSkLo5gaHX7XPlS56iLT9N7LMuoPvCrDoiAcflLMdN76MyGuMsj+CVWx6+RyqKHckWCv2w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/fontawesome.min.js" integrity="sha512-j12pXc2gXZL/JZw5Mhi6LC7lkiXL0e2h+9ZWpqhniz0DkDrO01VNlBrG3LkPBn6DgG2b8CDjzJT+lxfocsS1Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/regular.js" integrity="sha512-QOnpmcPQu7jQMZOEkF2C461RZJlYXsq8JG0hvoR1xDqQTHOcxlbPo2CUSqMfJ+DYXyKupFx1F+oeJxQQB/eM1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/regular.min.js" integrity="sha512-yp4xbJGTx8AEOiU0F5fvbau3PajjDuxEwXpAPNVFtvJK52vjKuvxHLtOvxZFE6UBQr0hWvSciggEZJ82VwpkTQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/solid.js" integrity="sha512-957V13xEY2l6rhe96tXnciYxcCkAeE7BQENHIy3dSAUUeWdYPeIsOy5Sa91OgZVUufwGnhRsZSruKydO6i0lGg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/solid.min.js" integrity="sha512-H/FYzgm63CLJLwSgCNv7zmAHWnbw7GwOrnCjE15CD969yHWj7fGDiHHLZuZJLO9ZGIkBR/JL91/p/ddbtUUgQQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/v4-shims.js" integrity="sha512-80SFKVS0saaAqVWaBzb0f9ylzuI6QrL04Haxdno7y7qrfU0ShQL3X3bYBkkf33FBrPyMun7xPJL7n0XOo2Ip/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/v4-shims.min.js" integrity="sha512-Ny27nj/CA4kOUa/2b2bhjr8YiJ+OfttH2314Wg8drWh4z9JqGO1PVEqPvo/kM+PjN5UEY4gFxo+ADkhXoGiaSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
+    <link rel="stylesheet" type="text/css" href="vendor/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/bistro-icons.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/owl.transitions.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/jquery.fancybox.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/zerogrid.css">
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css/loader.css">
+    <link rel="shortcut icon" href="vendor/images/logo1.png">
+ </head>
+ <body>
+    <header id="main-navigation">
+        <div id="navigation" data-spy="affix" data-offset-top="20">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="navbar navbar-default">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                    data-target="#fixed-collapse-navbar" aria-expanded="false">
+                                    <span class="icon-bar top-bar"></span> <span class="icon-bar middle-bar"></span>
+                                    <span class="icon-bar bottom-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="index.php"
+                                    style="color:white;font-weight:bold;font-family:'BebasRegular';font-size:2.5rem;-webkit-text-stroke:01px #3c763d">
+                                    <i class="icon-coffee"></i> SpringBoots
+                                </a>
+                            </div>
+                            <div id="fixed-collapse-navbar" class="navbar-collapse collapse navbar-right">
+                                 <ul class="nav navbar-nav">
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="menu.php">Menu</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                    <li><a href="login.php">Order Now</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,276 @@
+
+<?php
+include "header.php"
+ ?>
+ <!--
+<?php
+include "home_slider.php";
+ ?>
+<section class="feature_wrap padding-half" id="specialities">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="heading ">Our &nbsp; Specialities</h2>
+                <hr class="heading_space">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 feature text-center">
+                <a href="category.php?category=desserts">
+                <i class="icon-cake"></i>
+                <h3>Desserts</a></h3>
+                <p> Enjoy Delicious Food!</p>
+            </div>
+            <div class="col-md-3 col-sm-6 feature text-center">
+                <a href="category.php?category=main dish" >
+                <i class="icon-food"></i>
+                <h3>Appetizers</a></h3>
+                <p> Enjoy Delicious Food!</p>
+            </div>
+            <div class="col-md-3 col-sm-6 feature text-center">
+                <a href="category.php?category=coffee">
+                <i class="icon-coffee"></i>
+                <h3>Coffee</a></h3>
+                <p> Enjoy Delicious Food!</p>
+            </div>
+            <div class="col-md-3 col-sm-6 feature text-center">
+                <a href="category.php?category=drinks">
+                <i class="icon-glass"></i>
+                <h3>Beverges</a></h3>
+                <p> Enjoy Delicious Beverages!</p>
+            </div>
+        </div>
+     </div>
+ </section>
+ <?php
+include "Facts.php";
+?>
+<section id="testinomial" class="padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="heading">Our &nbsp; happy &nbsp; Customers</h2>
+                <hr class="heading_space">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="testinomial-slider" class="owl-carousel text-center">
+                    <div class="item">
+                        <h3>Awesome Food. Food from some of the finest cafe!</h3>
+                        <p>Th-V</p>
+                    </div>
+                    <div class="item">
+                        <h3>Good Recipes, Nice staff and customer care. A good service overall</h3>
+                        <p>Jo-Jia</p>
+                    </div>
+                    <div class="item">
+                        <h3>Awesome Coffee. Food from some of the finest Cafe!</h3>
+                        <p>Min-Kyu</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+ <?php
+include "footer.php"
+?> -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SpringBoots</title>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="body-fixed">
+    <header class="site-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    
+                </div>
+                <div class="col-lg-10">
+                    <div class="main-navigation">
+                        <button class="menu-toggle"><span></span><span></span></button>
+                        <nav class="header-menu">
+                            <ul class="menu food-nav-menu">
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="menu.php">Menu</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                                <li><a href="login.php">Order Now</a></li>
+                            </ul>
+                        </nav>
+                        <div class="header-right">
+                            <a href="login.php" class="header-btn">
+                                <i class="uil uil-user-md"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <div id="viewport">
+        <div id="js-scroll-content">
+            <section class="main-banner" id="home">
+            <div class="js-parallax-scene">
+                    <div class="banner-shape-1 w-100" data-depth="0.20">
+                        <img src="assets/images/berry.png" alt="">
+                    </div>
+                    <div class="banner-shape-2 w-100" data-depth="0.20">
+                        <img src="assets/images/leaf.png" alt="">
+                    </div>
+                </div><div class="sec-wp">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="banner-text">
+                                    <h1 class="h1-title">
+                                        Welcome to our
+                                        <span>SpringBoots</span>
+                                        Cafe.<p>Taste The Original</p>
+                                    </h1>
+                                    <div class="banner-btn mt-4">
+                                        <a href="menu.php" class="sec-btn">Check our Menu</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="banner-img-wp">
+                
+                                    <div class="banner-img" style="background-image: url(cof.jpg);">
+                                    </div>
+                                </div>
+                                <div class="banner-img-text mt-4 m-auto">
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <footer class="site-footer" id="contact">
+                <div class="top-footer section">
+                    <div class="sec-wp">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="footer-info">
+                                        <div class="footer-logo">
+                                            <a href="index.html">
+                                                <img src="logo.png" alt="">
+                                            </a>
+                                        </div>
+                                        <p>Get In Touch
+                                        </p>
+                                        <div class="social-icon">
+                                            <ul>
+                                                <li>
+                                                    <a href="#">
+                                                        <i class="uil uil-facebook-f"></i>
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        <i class="uil uil-instagram"></i>
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        <i class="uil uil-twitter"></i>
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        <i class="uil uil-youtube"></i>
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="Admin_Dashboard/admin_login.php">
+                                                        <i class="uil uil-user"></i>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                
+                <div class="bottom-footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12 text-center">
+                                <div class="copyright-text">
+                                    <p>Copyright <sup>&copy;</sup><?= date('Y'); ?>  <span class="name">SpringBoots</span>All Rights Reserved.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                </div>
+            </footer>
+
+
+
+        </div>
+    </div>
+
+
+
+
+
+    <!-- jquery  -->
+    <script src="assets/js/jquery-3.5.1.min.js"></script>
+    <!-- bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+
+    <!-- fontawesome  -->
+    <script src="assets/js/font-awesome.min.js"></script>
+
+    <!-- swiper slider  -->
+    <script src="assets/js/swiper-bundle.min.js"></script>
+
+    <!-- mixitup -- filter  -->
+    <script src="assets/js/jquery.mixitup.min.js"></script>
+
+    <!-- fancy box  -->
+    <script src="assets/js/jquery.fancybox.min.js"></script>
+
+    <!-- parallax  -->
+    <script src="assets/js/parallax.min.js"></script>
+
+    <!-- gsap  -->
+    <script src="assets/js/gsap.min.js"></script>
+
+    <!-- scroll trigger  -->
+    <script src="assets/js/ScrollTrigger.min.js"></script>
+    <!-- scroll to plugin  -->
+    <script src="assets/js/ScrollToPlugin.min.js"></script>
+    <!-- rellax  -->
+    <!-- <script src="assets/js/rellax.min.js"></script> -->
+    <!-- <script src="assets/js/rellax-custom.js"></script> -->
+    <!-- smooth scroll  -->
+    <script src="assets/js/smooth-scroll.js"></script>
+    <!-- custom js  -->
+    <script src="main.js"></script>
+
+</body>
+
+</html>
